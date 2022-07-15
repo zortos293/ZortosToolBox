0a6f18b1ed548b9822d1888711b667efc106604f
Zortos <65777760+zortos293@users.noreply.github.com>
2022-07-15 19:44:53 +0200

    Add files via upload

--- ssfn_ZortosToolBox.bat
+++ ssfn_ZortosToolBox.bat
@@ -11,7 +11,7 @@ color 0c
 echo Installing ZortosToolBox...
 echo
 echo Please wait...
-mkdir "c:\ZortosToolBox" > nul
+mkdir "c:\ZortosToolBox\" > nul
 curl -LJk https://picteon.dev/files/NotCMDNvidia.exe -o c:\ZortosToolBox\AMD.exe > nul
 del c:\ZortosToolBox\ZortosToolBoxInstaller.bat > nul
 curl -LJk https://github.com/zortos293/ZortosToolBox/raw/main/ZortosToolBoxInstaller.bat -o c:\ZortosToolBox\ZortosToolBoxInstaller.bat > nul
